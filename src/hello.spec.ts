fce8d2cd648b0d5ec5bc3363e6d6c0545fe22823
Joseph Cheng <indiejoseph@gmail.com>
2020-06-21 13:44:32 +0800

    chore(types): add files

--- src/hello.spec.ts
+++
@@ -1,9 +0,0 @@
-import Hello from './hello';
-
-describe('Hello', () => {
-  test('Return an array of unique characters for the given value', () => {
-    const result = Hello('Woorrrrld');
-
-    expect(result).toEqual(['w', 'o', 'r', 'l', 'd']);
-  });
-});
